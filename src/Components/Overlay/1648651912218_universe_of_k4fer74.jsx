2a0a4f04e6c9aa7f0ca863c6206bb2e0fcc5e9a0
Matheus Lugon <matheus@l4u.com.br>
2022-04-08 12:33:23 -0300

    class -> function

--- src/Components/Overlay/1648651912218_universe_of_k4fer74.jsx
+++ src/Components/Overlay/1648651912218_universe_of_k4fer74.jsx
@@ -15,17 +15,12 @@
 import React from "react"
 //import * as Styles from "./1648651912218_universe_of_k4fer74.module.css"
 
-export class Universe extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        return (
-            <div className={Styles.box}>
-                <img alt="" src={this.props.streamerDonate.animatedGIF} height={100} />
-                <h2><span className={Styles.highlight}>{this.props.streamerDonate.giver_username}</span> sent {this.props.streamerDonate.amount} {this.props.streamerDonate.currency}</h2>
-                <h3>{this.props.streamerDonate.giver_message}</h3>
-            </div>
-        )
-    }
+export function Universe(props) {
+    return (
+        <div className={Styles.box}>
+            <img alt="" src={this.props.streamerDonate.animatedGIF} height={100} />
+            <h2><span className={Styles.highlight}>{this.props.streamerDonate.giver_username}</span> sent {this.props.streamerDonate.amount} {this.props.streamerDonate.currency}</h2>
+            <h3>{this.props.streamerDonate.giver_message}</h3>
+        </div>
+    )
 }
