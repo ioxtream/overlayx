f408c70867c4fd3fafa3fe8af914b60c3b4ce5eb
vinicius <vinicius.souz@hotmail.com>
2022-07-07 17:35:18 -0300

    make IOXtream OverlayX responsive

--- src/Components/AlertBox/1657204205594_ioxtream_of_Zero-Nash-Tesla.js
+++ src/Components/AlertBox/1657204205594_ioxtream_of_Zero-Nash-Tesla.js
@@ -17,19 +17,20 @@ import Styles from "./1657204205594_ioxtream_of_Zero-Nash-Tesla.module.css"
 
 export default function IOXtream(props) {
     return (
-        <div>
+        <div className={Styles.container}>
+          <div className={Styles.box}>
+              <img alt="" src={props.donate.animated_gif} height={100} />
+              <div>
+                <h2><span className={Styles.highlight}>{props.donate.username}</span> sent {props.donate.amount} {props.donate.currency}</h2>
+                <h3>{props.donate.message}</h3>
+              </div>
+              <div className={Styles.io}>
+                  <img alt="" src="https://user-images.githubusercontent.com/98370909/171199876-b93f7ea9-f7fd-4320-9a44-091c78009c90.gif" width="100%"/>
+              </div>
+          </div>
           <div className={Styles.logo}>
             <img alt="" src="https://ioxtream.io/images/logo/ioxtream-beta.svg"/>
           </div>
-          <div className={Styles.box}>
-              <img alt="" src={props.donate.animated_gif} height={100} />
-              <h2><span className={Styles.highlight}>{props.donate.username}</span> sent {props.donate.amount} {props.donate.currency}</h2>
-              <h3>{props.donate.message}</h3>
-          </div>
-          <div className={Styles.io}>
-              <img alt="" src="https://user-images.githubusercontent.com/98370909/171199876-b93f7ea9-f7fd-4320-9a44-091c78009c90.gif" height={100}/>
-          </div>
         </div>
-
     )
 }
