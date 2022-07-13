9335cdc2c7665276d3cfadd1a5f91b8eca41d4da
vicaso <37910903+vicasoo@users.noreply.github.com>
2022-07-13 18:02:29 -0300

    Revert "Overlays responsivity refactor"

--- src/Donations.js
+++ src/Donations.js
@@ -1,6 +1,6 @@
 const SampleDonations = [{
-    username: "Dichlorodifluoromethanes",
-    message: "This overlayx contains the maximum characters in nickname and message --------------------------------------------------------------------------",
+    username: "Kverk0 Kverk0 Kverk0 aaaa",
+    message: "Thanks for sharing with us your knowledge!Thanks for sharing with us your knowledge!Thanks for sharing with us your knowledge! aaaaaaaaaaaaaaaaa",
     currency: "CHZ",
     amount: "4",
     animated_gif: "https://i.pinimg.com/originals/d6/6b/9c/d66b9cd46e19093e06e3edabdf21e1da.gif",
