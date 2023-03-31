bb764c894d3d43e2c25926c1248f891bdcb28c2a
vinicius <vinicius.souz@hotmail.com>
2023-03-31 18:54:37 -0300

    updating example

--- src/Donations.js
+++ src/Donations.js
@@ -1,8 +1,8 @@
 const SampleDonations = [{
     username: "Kverk0 Kverk0 Kverk0 aaaa",
     message: "Thanks for sharing with us your knowledge!Thanks for sharing with us your knowledge!Thanks for sharing with us your knowledge! aaaaaaaaaaaaaaaaa",
-    currency: "CHZ",
-    amount: "4",
+    currency: "BTC",
+    amount: "4 BTC",
     animated_gif: "https://i.pinimg.com/originals/d6/6b/9c/d66b9cd46e19093e06e3edabdf21e1da.gif",
 }]
 
